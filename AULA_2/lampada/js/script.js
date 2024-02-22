6d8c1d3b0cc2658741d609334b59ef2e17571910
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-02-22 11:05:37 -0300

    Add files via upload

---
+++ AULA_2/lampada/js/script.js
@@ -0,0 +1,34 @@
+const ligar = document.getElementById('ligar')
+const desligar = document.getElementById('desligar')
+const lampada = document.getElementById('lamp')
+
+function estaQuebrada(){
+    return lampada.src.indexOf('quebrada') > -1
+}
+
+function lampLigada(){
+    if(!estaQuebrada()){
+        lampada.src = "img/ligada.jpg"
+    }
+}
+
+function lampDesligada(){
+    if(!estaQuebrada()){
+        lampada.src = "img/desligada.jpg"
+    }
+
+}
+
+function lampQuebrada(){
+    lampada.src = "img/quebrada.jpg"
+
+
+}
+
+ligar.addEventListener('click',lampLigada)
+desligar.addEventListener('click',lampDesligada)
+
+lampada.addEventListener('mouseover',lampLigada)
+lampada.addEventListener('mouseleave',lampDesligada)
+
+lampada.addEventListener('dblclick',lampQuebrada)
