42a94c5926eb6611b4ac507570a39228147026f4
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-02-29 11:06:13 -0300

    Add files via upload

---
+++ AULA_3/calculadora/js/executa.js
@@ -0,0 +1,13 @@
+var resultado = document.getElementById('resultado')
+
+function recebeNome(){
+    var nome = document.getElementById('nome').value
+    
+    //console.log('Nome: ',nome)
+    resultado.innerHTML = nome
+}
+
+function recebeNumero(){
+    var num1 = document.getElementById('num1').value
+    var num2 = document.getElementById('num2').value
+}
