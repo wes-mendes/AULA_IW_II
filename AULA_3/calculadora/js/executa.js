c7f4fb635c18e3c3a15bd965df2179a993378991
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-02-29 21:45:30 -0300

    Add files via upload

--- AULA_3/calculadora/js/executa.js
+++ AULA_3/calculadora/js/executa.js
@@ -1,13 +1,29 @@
-var resultado = document.getElementById('resultado')
+var n1 = document.getElementById('n1');
+var n2 = document.getElementById('n2');
+var resultado = document.getElementById('resultado');
 
-function recebeNome(){
-    var nome = document.getElementById('nome').value
-    
-    //console.log('Nome: ',nome)
-    resultado.innerHTML = nome
-}
+        function somar() {
+            resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value);
+        }
 
-function recebeNumero(){
-    var num1 = document.getElementById('num1').value
-    var num2 = document.getElementById('num2').value
-}
+        function subtrair() {
+            resultado.innerHTML = parseFloat(n1.value) - parseFloat(n2.value);
+        }
+
+        function multiplicar() {
+            resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value);
+        }
+
+        function dividir() {
+            if (n2.value === "0") {
+              resultado.innerHTML = "Erro: Divisão por zero!";
+            } else {
+              resultado.innerHTML = parseFloat(n1.value) / parseFloat(n2.value);
+            }
+        }
+
+        function apagar() {
+            n1.value = "";
+            n2.value = "";
+            resultado.innerHTML = "";
+        }
