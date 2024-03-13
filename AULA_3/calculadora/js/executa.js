f46f33fb6442235cbe4e0b1b4fde0d9d938d2f20
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-03-12 22:18:02 -0300

    Add files via upload

--- AULA_3/calculadora/js/executa.js
+++ AULA_3/calculadora/js/executa.js
@@ -2,28 +2,28 @@ var n1 = document.getElementById('n1');
 var n2 = document.getElementById('n2');
 var resultado = document.getElementById('resultado');
 
-        function somar() {
-            resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value);
-        }
+function soma() {
+    resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value);
+}
 
-        function subtrair() {
-            resultado.innerHTML = parseFloat(n1.value) - parseFloat(n2.value);
-        }
+function subtracao() {
+    resultado.innerHTML = parseFloat(n1.value) - parseFloat(n2.value);
+}
 
-        function multiplicar() {
-            resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value);
-        }
+function multiplicacao() {
+    resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value);
+}
 
-        function dividir() {
-            if (n2.value === "0") {
-              resultado.innerHTML = "Erro: Divisão por zero!";
-            } else {
-              resultado.innerHTML = parseFloat(n1.value) / parseFloat(n2.value);
-            }
-        }
+function divisao() {
+    if (n2.value === "0") {
+      resultado.innerHTML = "Erro";
+    } else {
+      resultado.innerHTML = parseFloat(n1.value) / parseFloat(n2.value);
+    }
+}
 
-        function apagar() {
-            n1.value = "";
-            n2.value = "";
-            resultado.innerHTML = "";
-        }
+function limpar() {
+    n1.value = "";
+    n2.value = "";
+    resultado.innerHTML = "";
+}
